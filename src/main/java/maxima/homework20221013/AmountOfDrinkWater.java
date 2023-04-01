0d058b65b2a783e2ee5c0f282c38014a0bae7780
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-01 14:15:47 +1000

    Homework 2022-10-13

--- src/main/java/maxima/homework20221013/AmountOfDrinkWater.java
+++ src/main/java/maxima/homework20221013/AmountOfDrinkWater.java
@@ -8,8 +8,11 @@ public class AmountOfDrinkWater {
   }
 
   public static void main(String[] args) {
+    //Взять бутылку. В данном случае новую.
     Bottle bottle1 = new Bottle();
 
+    //Отобразить количество воды в бутылке.
+    // Так же можно было воспользоваться непосредственно методом getAmountOfWater() класса Bottle
     System.out.printf("For now we have %.1f litres of drink water.", getAvailableAmount(bottle1));
 
   }
