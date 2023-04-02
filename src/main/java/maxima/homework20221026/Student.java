bf2a26383e6a65587a07aade53be7ed8249bb65a
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-03 00:14:42 +1000

    Homework 2022-10-26

---
+++ src/main/java/maxima/homework20221026/Student.java
@@ -0,0 +1,36 @@
+package src.main.java.maxima.homework20221026;
+
+public class Student {
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private int age;
+  private String education;
+  private String hobby;
+
+  public Student(String firstName, String middleName, String lastName, int age, String education, String hobby) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.age = age;
+    this.education = education;
+    this.hobby = hobby;
+  }
+
+  @Override
+  public String toString() {
+    return "STUDENT INFO\n" +
+        "First name: " + firstName + '\n' +
+        "Middle name: " + middleName + '\n' +
+        "Last name: " + lastName + '\n' +
+        "Age: " + age + '\n' +
+        "Education: " + education + '\n' +
+        "Hobby: " + hobby + '\n';
+  }
+
+  public static void main(String[] args) {
+
+    Student student = new Student("Aleksei", "V", "Kazantsev", 43, "Self education", "Programming");
+    System.out.println(student);
+  }
+}
