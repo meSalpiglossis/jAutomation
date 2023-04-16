4a80bf8a504aaa6b867bd142493bbd294295e41d
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-16 22:10:07 +1000

    Homework 2022-12-04

---
+++ src/test/java/maxima/homework20221204/CalculatorNegativeTest.java
@@ -0,0 +1,17 @@
+package maxima.homework20221204;
+
+import maxima.homework20221116.Calculator;
+import org.testng.annotations.Test;
+
+import java.security.InvalidParameterException;
+
+public class CalculatorNegativeTest {
+
+    @Test(timeOut = 5000, expectedExceptions = InvalidParameterException.class)
+    public void divideBy0Test() {
+        Calculator calculator = new Calculator();
+        calculator.div(1, 0);
+    }
+
+
+}
