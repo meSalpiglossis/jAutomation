c286a467a71ef83c64921fa3a25b6194fe2ed594
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-04-19 23:53:09 +1000

    Homework 2022-12-04 edit

--- src/test/java/maxima/homework20221204/CalculatorNegativeTest.java
+++ src/test/java/maxima/homework20221204/CalculatorNegativeTest.java
@@ -2,22 +2,28 @@ package maxima.homework20221204;
 
 import maxima.homework20221116.Calculator;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.security.InvalidParameterException;
 
 public class CalculatorNegativeTest {
 
+    Calculator calculator;
+
+    @BeforeMethod
+    public void setup() {
+        calculator = new Calculator();
+    }
+
     @Test(timeOut = 5000, expectedExceptions = InvalidParameterException.class)
     public void divideBy0Test() {
-        Calculator calculator = new Calculator();
         calculator.div(1, 0);
     }
 
 
     @Test(timeOut = 5000, expectedExceptions = InvalidParameterException.class)
     public void sqrtNegativeNumberTest() {
-        Calculator calculator = new Calculator();
         calculator.sqrt(-1);
     }
 
